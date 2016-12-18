e9de8a03679ccb71e952d3624942f05faa85faa5
Derrick Showers <derrick@derrickshowers.com>
2016-12-18 13:43:18 -0800

    Initial commit

---
+++ NavigationController/DestinationViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DestinationViewController.swift
+//  NavigationController
+//
+//  Created by Derrick Showers on 12/18/16.
+//  Copyright © 2016 Derrick Showers. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DestinationViewController: UIViewController {
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Navigation Controllers
+
+Nothing special, just something quick to better understand how navigation controllers work in iOS.
